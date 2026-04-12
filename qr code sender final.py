fd3cb28a4290cc34a4bf0be316e9045a46d249d5
Skeleneon <amansuhail6849@gmail.com>
2026-04-11 23:14:30 -0700

    test

--- qr code sender final.py
+++ qr code sender final.py
@@ -55,7 +55,7 @@ def botSendMessage(msg, CHANNEL_ID):
     except Exception as e:
         log_error(f"Discord send failed: {e}")
 
-
+botSendMessage("Script started", EMAIL_CHANNEL_ID)
 # -----------------------------
 # LOAD PREVIOUS DATA (DICT SAFE)
 # -----------------------------
